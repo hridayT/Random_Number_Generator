38d3504f000c1b47b9e46f9b4f5fa49e86763ff6
hridayT <hriday.t@gmail.com>
2018-01-29 20:59:01 +0530

    Finally?

--- Lagrange.py
+++ Lagrange.py
@@ -7,13 +7,16 @@ Created on Thu Oct 12 08:36:11 2017
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def lagran(l):
-    x = np.linspace(-8,8,100)
+    x = np.linspace(-8, 8, 100)
     L = np.empty_like(x)
     for i in range(x.shape[0]):
         #print(x[i,0]*x[i,0]+ 1 - l*(x[i,0]*x[i,0] - 6*x + 8).shape)
-        L[i] = x[i]*x[i]+ 1 - l*(x[i]*x[i] - 6*x[i] + 8)
+        L[i] = x[i] * x[i] + 1 - l * (x[i] * x[i] - 6 * x[i] + 8)
     return L
 
+
+print("Change for Git")
 y = lagran(1000000)
-plt.plot(np.linspace(-8,8,100),y)
+plt.plot(np.linspace(-8, 8, 100), y)
